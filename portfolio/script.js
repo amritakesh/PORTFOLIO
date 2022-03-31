6b6f74fcbc5c5b7f5885560a37c7888d6d9c9804
amritakesh <amritaraneekesh@gmail.com>
2022-03-31 10:52:54 +0530

    Resume change

---
+++ portfolio/script.js
@@ -0,0 +1,13 @@
+  
+  window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+
+  
+     
+  
+ 
+  
+  
+  
